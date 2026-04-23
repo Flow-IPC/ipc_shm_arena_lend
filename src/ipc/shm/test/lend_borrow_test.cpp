459323f52c38076c0ab2d6309506ded3995de899
Yuri Goldfeld <yuri.goldfeld@yahoo.com>
2026-04-22 20:10:51 -0700

    (cont) (debug -- undo)

--- src/ipc/shm/test/lend_borrow_test.cpp
+++ src/ipc/shm/test/lend_borrow_test.cpp
@@ -76,7 +76,7 @@ namespace
 
   ipc::test::Test_logger g_logger_obj;
   Logger* const g_logger_console = &g_logger_obj;
-#if 0 //XXXno
+#if 1
   Logger* const g_logger = nullptr; // Normal: Flow-IPC objects silent.
 #else
   Logger* const g_logger = &g_logger_obj; // Flip for debugging.
